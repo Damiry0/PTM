0811bf2ed70785c47c53f2833f9c32e98912ed97
Damiry0 <55887851+Damiry0@users.noreply.github.com>
2021-03-17 13:59:45 +0100

    task 3,4 completed

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -20,13 +20,14 @@
 
 int main() {
 //zadanie 1
-	DDRD=0x11110000; //255 tez git
+/*	DDRD=0x11110000; //255 tez git
 
 	sbi(DDRD, PD6);
 
 	while (1) {
 		sbi(PORTD, PD6);
 		_delay_ms(2000);
+
 		cbi(PORTD, PD6);
 		_delay_ms(2000);
 	}
@@ -34,6 +35,64 @@ int main() {
 
 	for(int i=0;i<255;i++)
 	{
+
+	}*/
+
+//zadanie 3
+	DDRD = 0b11111111;
+	while(1)
+	{
+		PORTD=0b10000000;
+		_delay_ms(4000);
+		PORTD = 0b01000000;
+		_delay_ms(4000);
+		PORTD=0b00100000;
+		_delay_ms(4000);
+		PORTD = 0b00010000;
+		_delay_ms(4000);
+		PORTD=0b00001000;
+		_delay_ms(4000);
+		PORTD = 0b00000100;
+		_delay_ms(4000);
+		PORTD=0b00000010;
+		_delay_ms(4000);
+		PORTD = 0b00000001;
+		_delay_ms(4000);
+	}
+
+
+//zadanie 4
+	DDRD = 0b11111111;
+	while(1)
+	{
+		PORTD=0b10000000;
+		_delay_ms(4000);
+		PORTD = 0b01000000;
+		_delay_ms(4000);
+		PORTD=0b00100000;
+		_delay_ms(4000);
+		PORTD = 0b00010000;
+		_delay_ms(4000);
+		PORTD=0b00001000;
+		_delay_ms(4000);
+		PORTD = 0b00000100;
+		_delay_ms(4000);
+		PORTD=0b00000010;
+		_delay_ms(4000);
+		PORTD = 0b00000001;
+		_delay_ms(4000);
+				PORTD=0b00000010;
+				_delay_ms(4000);
+				PORTD = 0b00000100;
+				_delay_ms(4000);
+				PORTD=0b00001000;
+				_delay_ms(4000);
+				PORTD = 0b00010000;
+				_delay_ms(4000);
+				PORTD=0b00100000;
+				_delay_ms(4000);
+				PORTD = 0b01000000;
+				_delay_ms(4000);
 
 	}
 
